02e59fae750be6eaf0aeae49061ce5bbcbf7be07
deboonstra <daniel-boonstra@uiowa.edu>
2023-06-08 09:35:30 -0700

    Fixing directory_check documentation

--- R/directory_check.R
+++ R/directory_check.R
@@ -1,3 +1,5 @@
+#' Checking current working directory
+#' 
 #' Checking that current working directory has the sub-directories:
 #' `DATA_Iowa_activPAL`, `DATA_Pitt_activPAL`, and `DATA_WVU_activPAL`, 
 #' which contains all raw sleep diary and ActivPAL data.
@@ -8,16 +10,18 @@
 #' 
 #' @return Error message is returned if current working directory does not have 
 #' the appropriate sub-directories else nothing is returned.
+#' 
+#' @seealso `getwd()`, `dir()`
 #'
 #' @examples
-#' directory_check()
-#' directory_check(path = ".")
-#' directory_check(path = "../") # error
+#' # directory_check() # works if current directory has sub-directories
+#' # directory_check(path = ".") # works if current directory has sub-directories
+#' # directory_check(path = "../") # error by moving out of current directory
 #' 
 #' @export directory_check
 directory_check <- function(path = ".") {
-    setwd(path)
-    dirs <- dir()
+    base::setwd(path)
+    dirs <- base::dir()
     dirs_data <- c("DATA_Iowa_activPAL",  "DATA_Pitt_activPAL", "DATA_WVU_activPAL")
     if (!all(dirs_data %in% dirs)) {
         stop(
