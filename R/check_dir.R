d9ad49ff88c4d390062884ee38f25baf3c61b697
deboonstra <daniel-boonstra@uiowa.edu>
2023-06-24 18:27:53 -0700

    Changing function name from directory_check to check_dir

--- R/check_dir.R
+++ R/check_dir.R
@@ -1,25 +1,25 @@
 #' Checking current working directory
-#' 
+#'
 #' Checking that current working directory has the sub-directories:
-#' `DATA_Iowa_activPAL`, `DATA_Pitt_activPAL`, and `DATA_WVU_activPAL`, 
+#' `DATA_Iowa_activPAL`, `DATA_Pitt_activPAL`, and `DATA_WVU_activPAL`,
 #' which contains all raw sleep diary and ActivPAL data.
-#' 
-#' @param path A character vector of full path names; the default corresponds to 
-#' the working directory, `getwd()`. Also, `path` sets the working directory
+#'
+#' @param path A character vector of full path names; the default corresponds
+#' to the working directory, `getwd()`. Also, `path` sets the working directory
 #' with `setwd(path)`.
-#' 
-#' @return Error message is returned if current working directory does not have 
+#'
+#' @return Error message is returned if current working directory does not have
 #' the appropriate sub-directories else nothing is returned.
-#' 
-#' @seealso `getwd()`, `dir()`
+#'
+#'@seealso `getwd()`, `dir()`
 #'
 #' @examples
-#' # directory_check() # works if current directory has sub-directories
-#' # directory_check(path = ".") # works if current directory has sub-directories
-#' # directory_check(path = "../") # error by moving out of current directory
-#' 
-#' @export directory_check
-directory_check <- function(path = ".") {
+#' # check_dir() # works if current directory has sub-directories
+#' # check_dir(path = ".") # works if current directory has sub-directories
+#' # check_dir(path = "../") # error by moving out of current directory
+#'
+#' @export check_dir
+check_dir <- function(path = ".") {
     base::setwd(path)
     dirs <- base::dir()
     dirs_data <- c(
