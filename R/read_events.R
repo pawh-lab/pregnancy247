6372d46f58a274e2fc7906969da77f310be57bd4
deboonstra <daniel-boonstra@uiowa.edu>
2023-07-07 11:54:38 -0700

    Updating read_events to simplify the conditional

--- R/read_events.R
+++ R/read_events.R
@@ -1,38 +1,37 @@
-read_events <- function(subject, trimester) {
-  # Finding site location ####
-  data_site <- substr(subject, 1, 1)
+read_events <- function(subject, trimester, file = NULL, ...) {
 
-  if (data_site == "1") {
-    data_loc <- paste0(
-      "./DATA_Iowa_activPAL/",
-      subject, "/", subject, trimester
-    )
+  if (is.null(file)) {
+    # Finding site location ####
+    data_site <- substr(subject, 1, 1)
+
+    if (data_site == "1") {
+      data_loc <- paste0(
+        "./DATA_Iowa_activPAL/",
+        subject, "/", subject, trimester
+      )
+    } else if (data_loc == "5") {
+      data_loc <- paste0(
+        "./DATA_Pitt_activPAL/",
+        subject, "/", subject, trimester
+      )
+    } else if (data_loc == "8") {
+      data_loc <- paste0(
+        "./DATA_WVU_activPAL/",
+        subject, "/", subject, trimester
+      )
+    }
     files <- list.files(path = data_loc, pattern = "EventsEx.csv$")
     dat <- utils::read.csv(
       file = paste0(data_loc, "/", files),
-      skip = 2, header = FALSE, sep = ";", stringsAsFactors = FALSE
+      skip = 2, header = FALSE, sep = ";", stringsAsFactors = FALSE, ...
     )
-  } else if (data_loc == "5") {
-    data_loc <- paste0(
-      "./DATA_Pitt_activPAL/",
-      subject, "/", subject, trimester
-    )
-    files <- list.files(path = data_loc, pattern = "EventsEx.csv$")
+  } else {
     dat <- utils::read.csv(
-      file = paste0(data_loc, "/", files),
-      skip = 2, header = FALSE, sep = ";", stringsAsFactors = FALSE
-    )
-  } else if (data_loc == "8") {
-    data_loc <- paste0(
-      "./DATA_WVU_activPAL/",
-      subject, "/", subject, trimester
-    )
-    files <- list.files(path = data_loc, pattern = "EventsEx.csv$")
-    dat <- utils::read.csv(
-      file = paste0(data_loc, "/", files),
-      skip = 2, header = FALSE, sep = ";", stringsAsFactors = FALSE
+      file = file,
+      skip = 2, header = FALSE, sep = ";", stringsAsFactors = FALSE, ...
     )
   }
+
   class(dat) <- c("data.events", "data.frame")
   return(dat)
 }
