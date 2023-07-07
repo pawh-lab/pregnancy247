c230320138821e3a66d9cd60d500ebd14ea0ad78
deboonstra <daniel-boonstra@uiowa.edu>
2023-07-07 12:07:02 -0700

    Removing loc function from script file

--- R/write.R
+++ R/write.R
@@ -176,25 +176,3 @@ write_all_avgs <- function(x, subject, trimester, file = NULL, ...) {
       }
   }
 }
-
-# Helper location function ####
-loc <- function(subject, trimester) {
-  data_site <- substr(subject, 1, 1)
-  if (data_site == "1") {
-    data_loc <- paste0(
-      "./DATA_Iowa_activPAL/",
-      subject, "/"
-    )
-  } else if (data_site == "5") {
-    data_loc <- paste0(
-      "./DATA_Pitt_activPAL/",
-      subject, "/"
-    )
-  } else if (data_site == "8") {
-    data_loc <- paste0(
-      "./DATA_WVU_activPAL/",
-      subject, "/"
-    )
-  }
-  return(data_loc)
-}
