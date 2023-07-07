10302bb8c0db7df2dda184ebfe2bc781934021a6
deboonstra <daniel-boonstra@uiowa.edu>
2023-07-07 12:07:31 -0700

    Giving loc its own script file

---
+++ R/loc.R
@@ -0,0 +1,20 @@
+loc <- function(subject, trimester) {
+  data_site <- substr(subject, 1, 1)
+  if (data_site == "1") {
+    data_loc <- paste0(
+      "./DATA_Iowa_activPAL/",
+      subject, "/"
+    )
+  } else if (data_site == "5") {
+    data_loc <- paste0(
+      "./DATA_Pitt_activPAL/",
+      subject, "/"
+    )
+  } else if (data_site == "8") {
+    data_loc <- paste0(
+      "./DATA_WVU_activPAL/",
+      subject, "/"
+    )
+  }
+  return(data_loc)
+}
