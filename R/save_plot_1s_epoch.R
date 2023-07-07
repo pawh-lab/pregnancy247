9982ab8c125539b1bf2ed99998328cdd942da1b8
deboonstra <daniel-boonstra@uiowa.edu>
2023-07-07 12:04:03 -0700

    Using loc function to find data location

--- R/save_plot_1s_epoch.R
+++ R/save_plot_1s_epoch.R
@@ -1,23 +1,7 @@
 save_plot_1s_epoch <- function(x, subject, trimester, file = NULL, ...) {
   if (is.null(file)) {
-    data_site <- substr(subject, 1, 1)
     subject_id <- paste0(subject, trimester)
-    if (data_site == "1") {
-      data_loc <- paste0(
-        "./DATA_Iowa_activPAL/",
-        subject, "/"
-      )
-    } else if (data_site == "5") {
-      data_loc <- paste0(
-        "./DATA_Pitt_activPAL/",
-        subject, "/"
-      )
-    } else if (data_site == "8") {
-      data_loc <- paste0(
-        "./DATA_WVU_activPAL/",
-        subject, "/"
-      )
-    }
+    data_loc <- loc(subject, trimester)
     pdf(file = paste0(data_loc, subject_id, "_graph.pdf"), ...)
     plot(x)
     dev.off()
