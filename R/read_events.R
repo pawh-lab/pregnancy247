e13c5ed8aaa06de74073c457aa5abcc9e059a628
deboonstra <daniel-boonstra@uiowa.edu>
2023-07-07 12:01:30 -0700

    Using loc function to find data location

--- R/read_events.R
+++ R/read_events.R
@@ -1,25 +1,7 @@
 read_events <- function(subject, trimester, file = NULL, ...) {
 
   if (is.null(file)) {
-    # Finding site location ####
-    data_site <- substr(subject, 1, 1)
-
-    if (data_site == "1") {
-      data_loc <- paste0(
-        "./DATA_Iowa_activPAL/",
-        subject, "/", subject, trimester
-      )
-    } else if (data_loc == "5") {
-      data_loc <- paste0(
-        "./DATA_Pitt_activPAL/",
-        subject, "/", subject, trimester
-      )
-    } else if (data_loc == "8") {
-      data_loc <- paste0(
-        "./DATA_WVU_activPAL/",
-        subject, "/", subject, trimester
-      )
-    }
+    data_loc <- paste0(loc(subject, trimester), subject, trimester)
     files <- list.files(path = data_loc, pattern = "EventsEx.csv$")
     dat <- utils::read.csv(
       file = paste0(data_loc, "/", files),
