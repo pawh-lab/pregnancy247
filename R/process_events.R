0dfce50e2db77286d8a21103aab6bb68f5c41fd6
deboonstra <daniel-boonstra@uiowa.edu>
2023-07-09 11:08:25 -0700

    Writing documentation for process_events function

--- R/process_events.R
+++ R/process_events.R
@@ -1,3 +1,48 @@
+#' Manipulate the events data recorded by activPAL
+#'
+#' Manipulate and clean the events data record by activPAL and imported by
+#' `read_events()`.
+#'
+#' @param data A `data.events` object produced by `read_events()`.
+#'
+#' @details This functions rearranges the variables produced by the activPAL
+#' software and selects the variables related to:
+#' * `time`,
+#' * `datacount`,
+#' * `interval`,
+#' * `activity`,
+#' * `cummulativesteps`, and
+#' * `methrs`.
+#'
+#' The naming of variables is based on the archived package named
+#' [`activpalProcessing`](https://cran.r-project.org/src/contrib/Archive/activpalProcessing/)
+#' created by [Kate Lynden](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5469371/).
+#'
+#' Beyond variable selection, this function specifically:
+#' * doubles the `cumulativesteps` as the activPAL device is only on one leg,
+#' * transforms the `time` variable to a date-time (POSIXct) numeric vector
+#' based on the UTC time-zone, and
+#' * creates several new variables to indicate:
+#'    * wear day based on sleep onset (`wear_day`),
+#'    * sleep windows (`sleep_loop`),
+#'    * nap windows (`nap_loop`),
+#'    * wake windows (`wake_loop`), and
+#'    * work windows (`work_loop`)
+#'
+#' that are all missing values to be filled in later by the appropriate data.
+#'
+#' @return A `process.data.events` object, which is a `data.frame` containing
+#' the variables discussed in the **Details**.
+#'
+#' @seealso [lubridate::with_tz()]
+#'
+#' @examples
+#' \dontrun{
+#' dat <- read_events("0001-AB", "3")
+#' process_dat <- process_events(data = dat)
+#' }
+#'
+#' @export process_events
 process_events <- function(data) {
   if (!("data.events" %in% class(data))) {
     stop("data must be a data.events object imported by read_events")
