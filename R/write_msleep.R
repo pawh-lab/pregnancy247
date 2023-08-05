a91223c5da62058563020cc6fe301e31ffb21a2d
deboonstra <daniel-boonstra@uiowa.edu>
2023-08-05 11:37:45 -0500

    Including write_all_msleep and writing documentation

--- R/write_msleep.R
+++ R/write_msleep.R
@@ -1,3 +1,39 @@
+#' Export screened sleep or nap Actiwatch data
+#'
+#' Export screened subject-specific sleep or nap Actiwatch data to
+#' `subject`-specific directories.
+#'
+#' @name write_msleep
+#' 
+#' @inheritParams readr::write_csv
+#' @inheritParams read_msleep
+#' @param ... Any other parameters needed for [readr::write_csv()].
+#' 
+#' @details When `file` is the default value of `NULL` the `subject` and
+#' `trimester` parameters along with the current working directory are used to
+#' write the files that ends with
+#' * `_napsonly_flag.csv` if `nap = TRUE` and
+#' * `_restonly_flag.csv` by default.
+#'
+#' These functions performs more tasks than simply writing a data set. The
+#' `write_all_msleep` function looks for data sets that already exists within
+#' the project directory that contains a collection of wear period data based on
+#' the type of rest (sleep or nap) given by the `nap` parameter. For Pregnancy
+#' 24/7, the data sets file names are
+#' * `master_dataset_napsonly.csv` if `nap = TRUE` and
+#' * `master_dataset_restonly.csv` by default.
+#'
+#' If these files already exists, then the wear period data for the
+#' `subject` of interest during the `trimester` of interest is merged with the
+#' the existing data set based on the type of rest for the `subject`.
+#'
+#' @seealso [readr::write_csv()]
+#'
+#' @examples
+#' # See quality_check() for an example of how these functions are used.
+#'
+#' @rdname write_msleep
+#' @export write_msleep
 write_msleep <- function(x, subject, trimester, nap = FALSE, file = NULL, ...) {
   # Checking parameter values ####
   if (!is.character(subject)) {
@@ -28,4 +64,48 @@ write_msleep <- function(x, subject, trimester, nap = FALSE, file = NULL, ...) {
 
   # Exporting data ####
   readr::write_csv(x = x, file = ff, na = "", ...)
+}
+
+#' @rdname write_msleep
+#' @export write_all_msleep
+write_all_msleep <- function(x, subject, trimester, nap = FALSE, file = NULL, ...) { #nolint
+
+  # Determining file name ####
+  if (is.null(file)) {
+    if (nap) {
+      ff <- "./master_dataset_napsonly.csv"
+    } else {
+      ff <- "./master_dataset_restonly.csv"
+    }
+  } else {
+    ff <- file
+  }
+
+  # Exporting data ####
+  if (file.exists(ff)) {
+    ## Importing previous data to join with new data
+    temp <- utils::read.csv(
+      file = ff,
+      stringsAsFactors = FALSE,
+      na.string = ""
+    )
+    hold <- paste0(temp$subject_id, temp$trimester)
+    temp <- subset(temp, subset = paste0(subject, trimester) != hold)
+    temp <- dplyr::bind_rows(temp, x)
+    temp <- temp[order(temp$subject_id, temp$trimester), ]
+    readr::write_csv(
+      x = temp,
+      file = ff,
+      na = "",
+      ...
+    )
+  } else {
+    # Exporting data if it is the first collection
+    readr::write_csv(
+      x = x,
+      file = ff,
+      na = "",
+      ...
+    )
+  }
 }
